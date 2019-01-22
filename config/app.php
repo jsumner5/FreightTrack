d57b5494ada198cb001eb1fc2bb9d3e3792b6d33
Jerold Sumner <jeroldsumner@Jerolds-MacBook-Pro.local>
2019-01-21 19:08:49 -0500

    fix

--- config/app.php
+++ config/app.php
@@ -9,8 +9,8 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
-   // 'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+   // 'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
 
 
     /**
@@ -248,7 +248,7 @@ return [
 
     'Datasources' => [
 
-        'ss'/*'default'*/ => [
+        'ss' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
